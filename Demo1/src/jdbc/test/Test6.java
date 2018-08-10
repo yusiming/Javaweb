2c948cf076946d397ab504f58c1517d3b00cee0b
虞四明 <13966931523@163.com>
2018-08-10 11:29:58 +0800

    修改代码

--- Demo1/src/jdbc/test/Test6.java
+++ Demo1/src/jdbc/test/Test6.java
@@ -23,13 +23,13 @@ public class Test6 {
      * @return: boolean
      */
     private static boolean login1(String username, String password) throws ClassNotFoundException, SQLException {
-        //加载驱动类.(注册驱动),所有的java.sql.Driver的实现类都提供了static代码块，把自己注册到DriverManager中
+        // 加载驱动类.(注册驱动),所有的java.sql.Driver的实现类都提供了static代码块，把自己注册到DriverManager中
         Class.forName("com.mysql.cj.jdbc.Driver");
-        //得到连接对象
+        // 得到连接对象
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb1?useSSL=false", "root", "314159");
         Statement statement = connection.createStatement();
         System.out.println("select * from user where username='" + username + "' and password='" + password + "'");
-        //根据参数查询用户名密码是否正确
+        // 根据参数查询用户名密码是否正确
         ResultSet resultSet = statement.executeQuery("select * from user where username='" + username + "' and password='" + password + "'");
         return resultSet.next();
     }
--- Demo1/src/jdcp/MyConnection.java
+++ Demo1/src/jdcp/MyConnection.java
@@ -12,6 +12,7 @@ public class MyConnection extends ConnectionWrapper {
     public MyConnection(Connection con) {
         super(con);
     }
+
     public void close() throws SQLException {
         System.out.println("哈哈");
         super.close();
